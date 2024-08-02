6065f32d4720f91dcf841dd716090f8fed6e5f8f
klemberg <kobi_lemberg@intuit.com>
2024-08-03 00:27:38 +0300

    fix sbt

---
+++ project/Versions.scala
@@ -0,0 +1,6 @@
+object Versions {
+  lazy val `spark` = "3.5.0" //Local testing succeeded with cassandra 3.3.0, but need to test with Spark cluster
+  lazy val `config` = "1.3.3"
+  lazy val `mysql` = "8.0.33"
+  lazy val `testcontainers-scala` = "0.41.3"
+}
